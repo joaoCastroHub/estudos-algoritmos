cbab0330c9240973ee4791084f94a8f1d383b50f
TuTa <30981320+joaoCastroHub@users.noreply.github.com>
2024-06-28 17:07:18 +0000

    adjustment to javascript

--- index.js
+++ index.js
@@ -1,24 +1,77 @@
-'use strict';
-
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-let inputString: string = '';
-let inputLines: string[] = [];
-let currentLine: number = 0;
-process.stdin.on('data', function(inputStdin: string): void {
-    inputString += inputStdin;
-});
-
-process.stdin.on('end', function(): void {
-    inputLines = inputString.split('\n');
-    inputString = '';
-    main();
-});
-
-function readLine(): string {
-    return inputLines[currentLine++];
+var Tree = function() {
+    this.root = null;
 }
 
-function main() {
-    // Enter your code here
+Tree.prototype.insert = function(node, data) {
+    if (node == null){
+    	node = new Node(data);
+    }
+ 	else if (data < node.data){
+        node.left  = this.insert(node.left, data);
+    }
+    else{
+        node.right = this.insert(node.right, data);   
+    }
+
+    return node;
+}
+
+var Node = function(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+}
+
+/* head ends */
+
+/*
+    Node is defined as
+    var Node = function(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+*/
+
+// This is a "method-only" submission.
+// You only need to complete this method.
+
+function treeHeight(root) {
+	
+}
+
+/* tail begins */
+
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var _stdin = "";
+var _stdin_array = "";
+var _currentline = 0;
+
+process.stdin.on('data', function(data) {
+    _stdin += data;
+});
+
+process.stdin.on('end', function() {
+    _stdin_array = _stdin.split("\n");
+    solution();
+});
+
+function readLine() {
+    return _stdin_array[_currentline++];
+}
+
+function solution() {
+
+    var tree = new Tree();
+    var n = parseInt(readLine());
+
+    for (var i=0; i<n; i++) {
+        var m = parseInt(readLine());
+        tree.root = tree.insert(tree.root, m);
+    }
+
+    var height = treeHeight(tree.root);
+    process.stdout.write(height);
 }
